af6c76a59e3ade84dbb09780ccf79b58a18e99bd
maksonalves <141428273+maksonalves@users.noreply.github.com>
2023-09-01 12:29:32 -0300

    Add files via upload
    1°: Faça uma função que recebe por parâmetro o tempo de duração de uma fábrica expressa em segundos e retorna também por parâmetro esse tempo em horas, minutos e segundos
    2°: Faça uma função que recebe a idade de uma pessoa em anos, meses e dias e retorna essa idade expressa em dias
    3°: Faça uma função que recebe um valor inteiro e verifica se o valor é positivo ou negativo. Se positivo verificar se é par ou impar
    4°: Faça uma função que recebe por parâmetro o raio de uma esfera e calcula o seu volume (v = 4 / 3 * Pi * R³)
    5°: Faça uma função que receba um valor inteiro e positivo e calcula o seu fatorial
    6°: Escreva uma função que recebe por parâmetro um valor inteiro e positivo N e retorna o valor de S:
    S = 1 + ½ + 1/3 + ¼ + 1/5 + 1/N
    Respostas

---
+++ return.py
@@ -0,0 +1,30 @@
+#1°: Faça uma função que recebe por parâmetro o tempo de duração
+#de uma fábrica expressa em segundos e retorna também por parâmetro
+#esse tempo em horas, minutos e segundos.
+
+'''def calcular_tempo(seg):
+    horas = seg // 3600
+
+    seg -= horas * 3600
+
+    min = seg // 60
+
+    seg -= min * 60
+
+    return f"{horas} : {min} : {seg}"
+
+tempo = int(input("Digite o tempo total em segundos: "))
+
+print(calcular_tempo(tempo))
+'''
+
+#2°: Faça uma função que recebe a idade de uma pessoa em anos,
+#meses e dias e retorna essa idade expressa em dias
+
+def calcular_idade(dia)
+
+dia_ano = 360
+
+
+
+print(soma)
